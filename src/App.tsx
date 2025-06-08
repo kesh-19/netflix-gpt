600cb1842dccb6bcc285c4d0a6b2a61e8f6895bb
Shreyas Kelshikar <shreyaskelshikar19@gmail.com>
2025-06-08 20:28:51 +0530

    install styled-comps

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
-import React from "react";
+import { useState, useEffect } from "react";
+
 function App() {
-  return <div className="App">a</div>;
+  const [first, setFirst] = useState(0);
+
+  useEffect(() => {
+    setFirst(1);
+  }, []);
+
+  return <div className="App">{first}</div>;
 }
 
 export default App;
